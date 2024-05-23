4000fa92dd71dd64aeb5a60e1ca15f29aa4667a7
“I401” <milk7456852@gmail.com>
2024-05-23 13:36:04 +0800

    img route

--- src/views/LocatedBoard.vue
+++ src/views/LocatedBoard.vue
@@ -9,13 +9,19 @@
 </template>
 
 <script>
+import store1 from'@/assets/images/store-1.png';
+import store2 from'@/assets/images/store-2.png';
+import store3 from'@/assets/images/store-3.png';
+import store4 from'@/assets/images/store-4.png';
+import store5 from'@/assets/images/store-5.png';
+
   export default {
     data() {
       return {
         areas: ['請選擇區域', '台北市', '台中市', '高雄市'], 
         stores:[
           { 
-            image: '../src/assets/images/store-1.png',
+            image: store1,
             area: '台北市',
             name: '台北中山旗艦店',
             phone: '(02000)-1234',
@@ -26,7 +32,7 @@
             map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.4387437263845!2d121.51567918885503!3d25.05311440000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a96956d62c77%3A0xbd5001be159f4f21!2z5Lm-5p2vIOS4reWxseW6lw!5e0!3m2!1szh-TW!2stw!4v1715682706341!5m2!1szh-TW!2stw',
           },
           { 
-            image: '/store-2.png',
+            image: store2,
             area: '台北市',
             name: '台北綠園店',
             phone: '(02000)-2345',
@@ -37,7 +43,7 @@
             map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.0446251543167!2d121.53930068885496!3d25.032559600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ab53fa673baf%3A0x78a1c78d3b65765f!2z5aSn5a6J56uZ6YKj6YKKIOeyvue3u-eGseeCkg!5e0!3m2!1szh-TW!2stw!4v1715683074504!5m2!1szh-TW!2stw',
           },
           { 
-            image: '/store-3.png',
+            image: store3,
             area: '台中市',
             name: '台中清水旗艦店',
             phone: '(04000)-1234',
@@ -48,7 +54,7 @@
             map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7280.850347751182!2d120.67307630558673!3d24.156817310018788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d64450c2f5b%3A0xa63869c21cf2e300!2z5Lit5ZyL6Yar6Jel5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1715683461098!5m2!1szh-TW!2stw',
           },
           { 
-            image: '/store-4.png',
+            image: store4,
             area: '高雄市',
             name: '高雄中正形象店',
             phone: '(07000)-2345',
@@ -59,7 +65,7 @@
             map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58922.765037738085!2d120.25896722167968!3d22.62868999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e05e8365804f1%3A0xeee482c0796a9a3c!2z5a626YSJ5qiT!5e0!3m2!1szh-TW!2stw!4v1715683569261!5m2!1szh-TW!2stw',
           },
           { 
-            image: '/store-5.png',
+            image: store5,
             area: '高雄市',
             name: '高雄夢時代店',
             phone: '(07000)-1234',
